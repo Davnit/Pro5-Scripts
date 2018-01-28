aba7a40cf318c2b56b1fa55db701433ea6b507b7
Davnit <davnit31@gmail.com>
2018-01-28 07:31:13 -0500

    Add script to modify cue properties

---
+++ ModifyCues.py
@@ -0,0 +1,25 @@
+import sys
+from ParsePRO5 import *
+
+if len(sys.argv) < 4:
+    sys.exit("You must specify a file path, xpath, target property, and new value.")
+    
+target = sys.argv[1]    # The path to the document file
+xpath = sys.argv[2]     # The path to the node (under RVMediaCue) to be modified.
+property = sys.argv[3]  # The name of the property to modify.
+newValue = sys.argv[4]  # The new value to give the property.
+
+# Get document data
+tree = getDocumentTree(target)
+
+# Do this for every cue on every slide
+for slide in getSlides(tree.getroot()):
+    for cue in getCues(slide):
+        
+        # Do it for every element matching the given path
+        for element in cue.findall(xpath):
+            # Change the value
+            element.set(property, newValue)
+            
+# Save our changes
+tree.write(target)
